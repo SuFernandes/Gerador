09926ede43b544dc30776693b0a8dc8185a3d656
Suelen <ssfernandes788@gmail.com>
2022-05-29 19:53:10 -0300

    terceiro commit

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
-let sliderElement = document.querySelector("#slider");
-let buttonElement = document.querySelector("#button");
+let sliderElement = document.querySelector('#slider');
+let buttonElement = document.querySelector('#button');
 
-let sizePassword = document.querySelector("#valor");
-let password = document.querySelector("#password");
+let sizePassword = document.querySelector('#valor');
+let password = document.querySelector('#password');
 
-let containerPassword = document.querySelector("#container-password");
+let containerPassword = document.querySelector('#container-password');
 
-let charset = "abcdefghijklmnopqrstuvxwyzABCDEFGHIJKLMNOPQRSTUVXWYZ!@";
+let charset = "abcdefghijklmnopqrstuvxwyzABCDEFGHIJKLMNOPQRSTUVXWYZ!@"
 let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
 
-slider.oninput = function(){
+sliderElement.oninput = function(){
     sizePassword.innerHTML = this.value;
 }
 
@@ -22,15 +22,15 @@ function generatePassword(){
     for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
         pass += charset.charAt(Math.floor(Math.random() * n));
     }
-    containerPassword.classList.remove("hide");
-   password.innerHTML = pass;
 
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
 }
 
 function copyPassword(){
-    alert("Senha copiada com sucesso!");
+    alert("Senha copiada!")
     navigator.clipboard.writeText(novaSenha);
+    
 }
-
-
 
